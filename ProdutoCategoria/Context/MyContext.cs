ed23a1b262c3be9a2fbc4c1cb38c7b772251a377
Moises das Virgens Cruz Neto <moises.neto@ages.edu.br>
2022-03-20 23:19:25 -0300

    Adicionar arquivos de projeto.

---
+++ ProdutoCategoria/Context/MyContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProdutoCategoria.Controllers;
+using ProdutoCategoria.Modelos;
+
+namespace ProdutoCategoria.Context
+{
+    public class MyContext: DbContext
+    {
+        public MyContext(DbContextOptions<MyContext> opt) : base(opt)
+        {
+
+        }
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Produto> Produtos { get; set; }
+    }
+}
---
+++ ProdutoCategoria/Modelos/Produto.cs
@@ -0,0 +1,27 @@
+﻿using ProdutoCategoria.Modelos;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ProdutoCategoria.Controllers
+{
+    [Table("Produtos")]
+    public class Produto
+    {
+        [Key]
+        public int ProdutoId { get; set; }
+        [Required][MaxLength(80)]
+        public string Nome { get; set; }
+        [Required]
+        [MaxLength(200)]
+        public string Descricao { get; set; }
+        public decimal Preco { get; set; }
+        [Required]
+        [MaxLength(500)]
+        public string ImgUrl { get; set; }
+        public float Estoque { get; set; }
+        public DateTime  Caadastro { get; set; }
+        public Categoria Categoria { get; set; }
+        public int CategoriaId { get; set; }
+    }
+}
